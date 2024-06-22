54e9c529a986e0c1bbb0f8425fac9872910da8a3
ldh9669 <leedonghee960@gmail.com>
2024-06-22 23:19:34 +0900

    fix: styled-components iussue about SSR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import '../styles/base/_reset.scss';
 import '../styles/constants/_colors.scss';
+import StyledComponentsRegistry from './lib/registry';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
---
+++ src/styles/global-styles.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+// eslint-disable-next-line import/prefer-default-export
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+
+  html, body { 
+    width: 100%;
+    height: 100%;
+    min-width: 1200px;
+    background-color: #ffffff;
+    font-family:-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI;
+    font-size : 16px;
+    color: rgb(58, 58, 58);
+  }
+
+  ul, ol {
+    list-style: none;
+  } 
+
+  button {
+    all: unset;
+    cursor: pointer;
+  }
+`;
