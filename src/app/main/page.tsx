de2ba7002f5b6444177323f9d5e7c58b75aa8e27
Song Jae Won <jwsonghub@gmail.com>
2024-07-01 11:34:03 +0900

    fix: merge conflict search->search

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './main.scss';
 import '../../styles/base/_reset.scss';
-import Navbar from '../../components/Navbar/Navbar';
+import Navbar from '@/components/Navbar/Navbar';
+import Footer from '@/components/Footer';
+import MainBanner from '@/components/MainBanner';
 
 const page = () => {
   return (
     <>
       <Navbar />
+      <Footer />
+      <MainBanner />
       <div>test</div>
     </>
   );
---
+++ src/components/MainSearch.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import React from 'react';
+// import DatePicker from 'react-datepicker';
+// import dayjs from 'dayjs';
+import styled from 'styled-components';
+
+function MainSearch(): React.JSX.Element {
+  return (
+    <div>
+      <SearchWrapper>
+        <SearchTitle>검색하기</SearchTitle>
+        <SearchElementsWrapper>
+          <p>지역</p>
+          <SearchSelector>지역 선택하기</SearchSelector>
+        </SearchElementsWrapper>
+        <SearchElementsWrapper>
+          <p>인원</p>
+          <SearchSelector>인원수 선택하기</SearchSelector>
+        </SearchElementsWrapper>
+        <SearchElementsWrapper>
+          <p>날짜</p>
+          <SearchSelector>체크인, 체크아웃 날짜 선택하기</SearchSelector>
+        </SearchElementsWrapper>
+      </SearchWrapper>
+    </div>
+  );
+}
+
+const SearchWrapper = styled.div`
+  width: 750px;
+  padding: 40px;
+  background: rgba(255, 255, 255, 0.65);
+`;
+
+const SearchTitle = styled.div`
+  font-size: 30px;
+  font-weight: 700;
+  padding-bottom: 50px;
+`;
+
+const SearchElementsWrapper = styled.div`
+  width: 750px;
+  padding-bottom: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  p {
+    flex-grow: 0.5;
+    flex-basis: 0;
+    font-size: 23px;
+  }
+  div {
+    flex-grow: 3;
+    flex-basis: 0;
+    height: 50px;
+    background-color: #ffffff;
+  }
+`;
+
+const SearchSelector = styled.div`
+  
+`;
+
+export default MainSearch;
